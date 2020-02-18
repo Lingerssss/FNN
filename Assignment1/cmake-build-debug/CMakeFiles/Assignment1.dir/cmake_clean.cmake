9c9dafca8ae486a4ff8d8ec12298da72e90a7277
巩嘉熹 <gongjiaxi@gongjiakitekiMacBook-Air.local>
2020-02-18 23:53:49 +0800

    qwertyuiop[

---
+++ Assignment1/cmake-build-debug/CMakeFiles/Assignment1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Assignment1"
+  "Assignment1.pdb"
+  "CMakeFiles/Assignment1.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Assignment1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
