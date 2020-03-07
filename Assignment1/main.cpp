bf5580f8cda9977db341697e770452d96f428592
Lingerssss <136810939@qq.com>
2020-03-07 16:59:06 +0800

    Merge branch 'master' of https://github.com/Lingerssss/ANN
    # Conflicts:
    #	README.md
    1.终于拆开了函数。
    2.结果已经可以了，但back过程还需改进。
    3.free加上去就崩。
    4.加入了int_string 和string_int，为以后变量名自增做准备。
    5.迟早转成类来实现任意自动n层隐藏层。
    
    Signed-off-by: Lingerssss <136810939@qq.com>

--- Assignment1/main.cpp
+++ Assignment1/main.cpp
@@ -12,6 +12,7 @@
 #include<cstdio>
 #include<cmath>
 #include<ctime>
+#include<sstream>
 
 using namespace std;
 
@@ -20,34 +21,75 @@ const int MAXPATS = 5000;  // Max training patterns
 
 // mlp paramaters
 long  NumIts ;    // Max training iterations
-int   NumHN  ;    // Number of hidden layers
-int   NumHN1 ;    // Number of neurons in hidden layer 1
-int   NumHN2 ;    // Number of neurons in hidden layer 2
-int   NumHN3 ;    // Number of neurons in hidden layer 3
-int   NumHN4 ;    // Number of neurons in hidden layer 4
+int NumHN;    // Number of hidden layers
+int NumHN1;    // Number of neurons in hidden layer 1
+int NumHN2;    // Number of neurons in hidden layer 2
+int NumHN3;    // Number of neurons in hidden layer 3
+int NumHN4;    // Number of neurons in hidden layer 4
 float LrnRate;    // Learning rate
-float Mtm1   ;    // Momentum(t-1)
-float Mtm2   ;    // Momentum(t-2)
-float ObjErr ;    // Objective error
+float Mtm1;    // Momentum(t-1)
+float Mtm2;    // Momentum(t-2)
+float ObjErr;    // Objective error
+
+int CurrentHN;
 
 // mlp weights
-float **w1,**w11,**w111;// 1st layer wts
-float **w2,**w22,**w222;// 2nd layer wts
+float **w1, **w11, **w111;// 1st layer wts
+float **w2, **w22, **w222;// 2nd layer wts
+
+double MinErr;
+double MaxErr;
+double AveErr;
+double NumErr;
+double PatErr;
 
 void TrainNet(float **x, float **d, int NumIPs, int NumOPs, int NumPats);
-void TrainNet3(float **x, float **d, int NumIPs, int NumOPs, int NumPats);
-void TestNet(float **x,float **d,int NumIPs,int NumOPs,int NumPats);
-float **Aloc2DAry(int m,int n);
-void Free2DAry(float **Ary2D,int n);
 
-int main(){
+void TrainNet3(float **x, float **d, int NumIPs, int NumOPs, int NumPats);
+
+void TestNet(float **x, float **d, int NumIPs, int NumOPs, int NumPats);
+
+float **Aloc2DAry(int m, int n);
+
+void Free2DAry(float **Ary2D, int n);
+
+
+int CHN;
+float temp;
+
+void Weight(int Numx, int Numy);
+
+void County(float **x, float **temp, int Numx, int Numy, int NumPats);
+
+void CountErr(float **d, float **temp, int NumIPs, int NumOPs, int NumPats);
+
+void back(float **d, float **temp, int Numx, int Numy, int NumPats, int CHN);
+
+
+string int_string(int i, string s);
+
+int string_int(string s, int i);
+
+
+float **hh = Aloc2DAry(NumHN, MAXN);
+float **h1, **h2, **h3, **h4, **h5, **h6, **h7, **h8, **h9;
+
+
+int NumTrnPats, NumTstPats, Ordering;
+
+
+int main() {
     ifstream fin;
-    int i,j,NumIPs,NumOPs,NumTrnPats,NumTstPats,Ordering;
-    char Line[500],Tmp[20],FName[20];
-    cout<<"Enter data filename: ";
-    cin>>FName; cin.ignore();
-    fin.open("/Users/gongjiaxi/CLionProjects/Assignment1/data1.txt");
-    if(!fin.good()){cout<<"File not found!\n";exit(1);}
+    int i, j, NumIPs, NumOPs, NumTrnPats, NumTstPats, Ordering;
+    char Line[500], Tmp[20], FName[20];
+    cout << "Enter data filename: ";
+    cin >> FName;
+    cin.ignore();
+    fin.open("/Users/gongjiaxi/CLionProjects/Assignment1/data2.txt");
+    if (!fin.good()) {
+        cout << "File not found!\n";
+        exit(1);
+    }
     //read data specs...
     do{fin.getline(Line,500);}while(Line[0]==';'); //eat comments
     sscanf(Line,"%s%d",Tmp,&NumIPs);
@@ -88,13 +130,12 @@ int main(){
             fin>>OPTstData[i][j];
     }
     fin.close();
-    float *A=new float [NumHN];
-    NumHN[0]= ;
+
 
     TrainNet3(IPTrnData,OPTrnData,NumIPs,NumOPs,NumTrnPats);
     //TrainNet(IPTrnData,OPTrnData,NumIPs,NumOPs,NumTrnPats);
-    //TestNet(IPTstData,OPTstData,NumIPs,NumOPs,NumTstPats);
-    Free2DAry(IPTrnData,NumTrnPats);
+    TestNet(IPTstData, OPTstData, NumIPs, NumOPs, NumTstPats);
+    Free2DAry(IPTrnData, NumTrnPats);
     Free2DAry(OPTrnData,NumTrnPats);
     Free2DAry(IPTstData,NumTstPats);
     Free2DAry(OPTstData,NumTstPats);
@@ -189,38 +230,48 @@ int main(){
             }
         }// end for each pattern
         ItCnt++;
-        AveErr/=NumPats;
-        float PcntErr = NumErr/float(NumPats) * 100.0;
-        cout.setf(ios::fixed|ios::showpoint);
-        cout<<setprecision(6)<<setw(6)<<ItCnt<<": "<<setw(12)<<MinErr<<setw(12)<<AveErr<<setw(12)<<MaxErr<<setw(12)<<PcntErr<<endl;//
+        AveErr /= NumPats;
+        float PcntErr = NumErr / float(NumPats) * 100.0;
+        cout.setf(ios::fixed | ios::showpoint);
+        cout << setprecision(6) << setw(6) << ItCnt << ": " << setw(12) << MinErr << setw(12) << AveErr << setw(12)
+             << MaxErr << setw(12) << PcntErr << endl;//
 
-        if((AveErr<=ObjErr)||(ItCnt==NumIts)) break;
+        if ((AveErr <= ObjErr) || (ItCnt == NumIts)) break;
     }// end main learning loop
-    // Free memory
-    delete h1; delete y;
-    delete ad1; delete ad2;
+     // Free memory
+     delete[] h1;
+     delete[] y;
+     delete[] ad1;
+     delete[] ad2;
 
-}
+ }
 
-void TrainNet3(float **x, float **d, int NumIPs, int NumOPs, int NumPats){
-TrainNet(x, d, NumIPs, NumOPs, NumPats);
-}
-
-void County(float **x,int Numx,int Numy,int NumPats ,int CHN,int HN){
-    int i,j,p;
-    float *y  = new float[Numy];
-    float *H = new float[HN];
-    for(p=0;p<NumPats;p++) { // for each pattern...
-        // Cal neural network output
-        for (i = 0; i < Numy; i++) { // Cal O/P of hidden layer 1
-            float in = 0;
-            for (j = 0; j < Numx; j++)
-                in += w1[j][i] * x[p][j];
-            y[i] = (float) (1.0 / (1.0 + exp(float(-in))));// Sigmoid fn
-
-        }
+void TrainNet3(float **x, float **d, int NumIPs, int NumOPs, int NumPats) {
+    int ItCnt = 0;
+    for (;;) {// Main learning loop
+        MinErr = 3.4e38;
+        AveErr = 0;
+        MaxErr = -3.4e38;
+        NumErr = 0;
+        Aloc2DAry(NumHN, MAXN);// 1st layer wts
+        Weight(NumIPs, NumHN1);
+        Weight(NumHN1, NumOPs);
+        County(x, h1, NumIPs, NumHN1, NumTrnPats);
+        County(h1, h2, NumHN1, NumHN2, NumTrnPats);
+        CountErr(d, h2, NumIPs, NumOPs, NumTrnPats);
+        back(d, h2, NumIPs, NumOPs, NumTrnPats, 1);
+        back(h2, h1, NumIPs, NumOPs, NumTrnPats, 0);
+        ItCnt++;
+        AveErr /= NumPats;
+        float PcntErr = NumErr / float(NumPats) * 100.0;
+        cout.setf(ios::fixed | ios::showpoint);
+        cout << setprecision(6) << setw(6) << ItCnt << ": " << setw(12) << MinErr << setw(12) << AveErr << setw(12)
+             << MaxErr << setw(12) << PcntErr << endl;//
+        if ((AveErr <= ObjErr) || (ItCnt == NumIts)) break;
     }
+    TrainNet(x, d, NumIPs, NumOPs, NumPats);
 }
+
 void Weight(int Numx,int Numy) {
     // Allocate memory for weights
     w1 = Aloc2DAry(Numx, Numy);// 1st layer wts
@@ -234,14 +285,56 @@ void Weight(int Numx,int Numy) {
             w1[i][j] = w11[i][j] = w111[i][j] = float(rand()) / RAND_MAX - 0.5;
 }
 
-void back(float **d,int Numx,int Numy,int NumPats,float *y){
-    int i,j,p;
-    for(p=0;p<NumPats;p++) {
+void County(float **x, float **temp, int Numx, int Numy, int NumPats) {
+    int i, j, p;
+    string s;
+    temp = Aloc2DAry(NumPats, Numy);
+    // for each pattern...
+    // Cal neural network output
+    for (p = 0; p < NumPats; p++) {
+        for (i = 0; i < Numy; i++) { // Cal O/P of hidden layer 1
+            float in = 0;
+            for (j = 0; j < Numx; j++)
+                in += w1[j][i] * x[p][j];
+            temp[p][i] = (float) (1.0 / (1.0 + exp(float(-in))));// Sigmoid fn
+        }
+    }
+}
+
+
+// Cal error for this pattern
+void CountErr(float **d, float **temp, int NumIPs, int NumOPs, int NumPats) {
+    MinErr = 3.4e38;
+    AveErr = 0;
+    MaxErr = -3.4e38;
+    NumErr = 0;
+    PatErr = 0.0;
+    int i, j, p;
+    for (p = 0; p < NumPats; p++) {
+        for (i = 0; i < NumOPs; i++) {
+            float err = temp[NumHN - 1][i] - d[p][i]; // actual-desired O/P
+            if (err > 0)PatErr += err; else PatErr -= err;
+            NumErr += ((temp[NumHN - 1][i] < 0.5 && d[p][i] >= 0.5) ||
+                       (temp[NumHN - 1][i] >= 0.5 && d[p][i] < 0.5));//added for binary classification problem
+        }
+        if (PatErr < MinErr)MinErr = PatErr;
+        if (PatErr > MaxErr)MaxErr = PatErr;
+        AveErr += PatErr;
+    }
+
+}
+
+
+void back(float **d, float **temp, int Numx, int Numy, int NumPats, int CHN) {//CHN是隐藏层层数，这里hh[CHN+1][I]表示的是前置过程中算出的Y
+
+    int i, j, p;
+
+    for (p = 0; p < NumPats; p++) {
         float *ad = new float[Numy];
         for (i = 0; i < Numy; i++) { // Modify layer 2 wts
-            ad[i] = (d[p][i] - y[i]) * y[i] * (1.0 - y[i]);
+            ad[i] = (d[p][i] - temp[CHN + 1][i]) * temp[CHN + 1][i] * (1.0 - temp[CHN + 1][i]);
             for (j = 0; j < Numx; j++) {
-                w1[j][i] += LrnRate * h1[j] * ad[i] +
+                w1[j][i] += LrnRate * temp[CHN][j] * ad[i] +
                             Mtm1 * (w1[j][i] - w11[j][i]) +
                             Mtm2 * (w11[j][i] - w111[j][i]);
                 w111[j][i] = w11[j][i];
@@ -276,8 +369,6 @@ void TestNet(float **x,float **d,int NumIPs,int NumOPs,int NumPats ) {
                 y[i]=(float)(1.0/(1.0+exp(float(-in))));// Sigmoid fn
                 cout<<setprecision(6)<<setw(6)<<h1[i]<<": "<<setw(12)<<y[i]<<endl;//
             }
-
-
     }
 }
 float **Aloc2DAry(int m,int n){
@@ -290,10 +381,28 @@ float **Aloc2DAry(int m,int n){
     }
     return Ary2D;
 }
-void Free2DAry(float **Ary2D,int n){
+
+void Free2DAry(float **Ary2D, int n) {
 //Frees memory in 2D array
-    for(int i=0;i<n;i++)
-        delete [] Ary2D[i];
-    delete [] Ary2D;
+    for (int i = 0; i < n; i++)
+        delete[] Ary2D[i];
+    delete[] Ary2D;
 }
 
+
+string int_string(int i) {
+    stringstream stream;
+    string s;
+    stream << i;
+    stream >> s;
+    return s;
+
+}
+
+int string_int(string s) {
+    stringstream stream;
+    int i;
+    stream << s;
+    stream >> i;
+    return i;
+}
---
+++ Test3.6/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include<stdlib.h>
+
+int main() {
+    int h,i=0;
+    char *s;
+    s = "h" + std::to_string(i++);
+    for(int j=0;j<10;j++)
+    h=atoi(s)
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
